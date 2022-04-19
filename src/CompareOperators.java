ccaca4ec95f08e00da2d7321887b514ef5031de4
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-04-19 22:51:11 +0200

    Operatory java

---
+++ src/CompareOperators.java
@@ -0,0 +1,15 @@
+public class CompareOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        System.out.println("Czy X jest większe od Y?");
+        System.out.println(x>y);
+
+        System.out.println("Czy X jest różny od Y?");
+        System.out.println(x != y);
+
+        boolean instance = "Kasia" instanceof java.lang.String;
+        System.out.println(instance);
+
+    }
+}
---
+++ src/MathOperators2.java
@@ -0,0 +1,9 @@
+public class MathOperators2 {
+    public static void main(String[] args) {
+        double x = 10;
+        int y = 4;
+        double div = x/y;
+
+        System.out.println(div);
+    }
+}
