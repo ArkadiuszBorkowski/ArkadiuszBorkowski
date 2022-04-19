923ceab8e4df6dddf6537f7da420a93c299345d5
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-04-19 23:08:43 +0200

    operatory java

--- src/LogicalOperators.java
+++ src/LogicalOperators.java
@@ -1,2 +1,23 @@
 public class LogicalOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y= 10;
+        boolean logic = true;
+
+        System.out.println("x<10 && y<11?");
+        System.out.println(x < 10 && y < 11);
+
+        System.out.println("x<10 && y<10");
+        System.out.println(x < 10 && y < 10);
+
+        System.out.println("x<10 || y<10");
+        System.out.println(x<10 || y<10);
+
+        System.out.print(logic);
+        System.out.print(" , !logic = ");
+        System.out.println(!logic);
+
+        System.out.print("Warunek złożony !(x<10 && y<10) ");
+        System.out.println(!(x < 10 && y < 10));
+    }
 }
